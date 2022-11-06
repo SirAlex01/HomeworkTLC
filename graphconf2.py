66039cee8b4c7892e330372fc71cd42987491c8b
DMan001 <bartali.coppi@gmail.com>
2022-11-06 12:18:28 +0100

    Finite domande extra con grafici

--- graphconf2.py
+++ graphconf2.py
@@ -2,23 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
-df1 = pd.read_csv("10.csv")
+df1 = pd.read_csv("conf2.csv")
 size1=df1["Length"].tolist()
 avg_size=0
 joined_size=size1
 for size in joined_size:
     avg_size=avg_size+size
 avg_size=avg_size/len(joined_size)
-time1=df1["Time"].tolist()
-time=time1
+time=df1["Time"].tolist()
 x=[]
 y=[]
-time_0=time[0]
 check_time=time[0]
 r=0
-for i in range(1,len(joined_size)):
-    r=r+1
-    if time[i]-check_time<=0.25:
+for i in range(0,len(joined_size)):
+    r=r+(size1[i]/avg_size)
+    if time[i]-check_time<=0.3:
         continue
     #print((r/(time[i]-check_time))*(avg_size/80000000),r,time[i])
     I=(r/(time[i]-check_time))*(avg_size/83886080)
@@ -27,7 +25,7 @@ for i in range(1,len(joined_size)):
     check_time=time[i]
     r=0
 
-
+#print(x,y)
 
 #fig = go.Figure(
     #data=[go.Line(x=x, y=y)],
